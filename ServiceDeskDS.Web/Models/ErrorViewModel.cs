0f061cdebedac61a1d5076eee133a65077e912a5
Deivisson Santos <deivissonsantos@hotmail.com>
2023-07-25 21:37:24 -0300

    [FEATURE] inclusao de estrutura inicial

---
+++ ServiceDeskDS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServiceDeskDS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
